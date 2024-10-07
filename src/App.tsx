2c846974ca308935c5af6dc3517e440ad50ca7f7
xbakbal <burak.akbal@windowslive.com>
2024-10-07 22:48:49 +0300

    UI Changes & Bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import crowbarLogo from './assets/crowbar.png'
+import instagramLogo from './assets/instagram.svg'
+import facebookLogo from './assets/facebook.svg'
+import youtubeLogo from './assets/youtube.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+      <a href="https://teknolojiklevye.com" target="_blank">
+        <img src={crowbarLogo} className="mainlogo crowbar" alt="Crowbar logo" />
+      </a>
+      <h1>Arka planda bol bol levye sallıyoruz...</h1>
+      <h1>Sosyal medyada gözünüz üzerimizde olsun! </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={instagramLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={facebookLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={youtubeLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Hello!
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
