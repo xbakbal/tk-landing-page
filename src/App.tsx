5cdfd520b6883ff01ad303ce797d4322b2b254c1
xbakbal <burak.akbal@windowslive.com>
2024-10-15 13:35:54 +0300

    new logo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import crowbarLogo from './assets/crowbar.png'
+import crowbarLogo from './assets/tk-logo.png'
 import instagramLogo from './assets/instagram.svg'
 import facebookLogo from './assets/facebook.svg'
 import youtubeLogo from './assets/youtube.svg'
@@ -6,9 +6,21 @@ import tiktokLogo from './assets/tiktok.svg'
 import twitterLogo from './assets/twitter.svg'
 import './App.css'
 
-function App() {
-  // const [count, setCount] = useState(0)
+// function App() {
+//   // const twitterAccount = "https://twitter.com/TeknolojikLevye/status/";
+//   // const latestTweet = "1844054497414496740";
 
+//   // const tweetLink = twitterAccount + latestTweet;
+
+//   return (
+//     <div style={{ display: 'flex', justifyContent: 'center' }}>
+//       {/* <XEmbed url={tweetLink} width={325}/> */}
+//       <InstagramEmbed url="https://www.instagram.com/p/DA6VNryNzrn/" width={328} captioned />
+//     </div>
+//   )
+// }
+
+function App() {
   return (
     <>
       <a href="https://teknolojiklevye.com" target="_blank">
@@ -33,6 +45,9 @@ function App() {
           <img src={tiktokLogo} className="logo" alt="Tiktok logo" />
         </a>
       </div>
+      {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <XEmbed url="https://x.com/TeknolojikLevye/status/1843415762234413153" width={325} />
+      </div> */}
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
